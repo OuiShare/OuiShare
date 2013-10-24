537ccf5c137c395b083608d1f9b8d96f5592d305
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-10-24 12:19:43 -0500

    Configure Devise helper for specs controllers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.include Devise::TestHelpers, type: :controller
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
