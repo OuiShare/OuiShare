76388db5d773c0d936f9568c9637340829cbf381
Josemar Davi Luedke <josemarluedke@gmail.com>
2013-10-24 12:19:48 -0500

    Add Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 OuiShare::Application.routes.draw do
+  root :to => "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
