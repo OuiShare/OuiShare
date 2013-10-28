b2c65e4cb6f56d4547813d5eca3b5b81a5c2e4da
Rafael Barboza <barboza.cardoso@gmail.com>
2013-10-28 14:49:16 -0200

    adds the admin field to the user model
    Only users with admin privileges will be able to edit the admin section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131024171952) do
+ActiveRecord::Schema.define(version: 20131024181037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,8 @@ ActiveRecord::Schema.define(version: 20131024171952) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "image"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20131024171952) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
