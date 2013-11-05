dfa9fbdf17786dd401472d009af580fab29aabbf
Rafael Barboza <barboza.cardoso@gmail.com>
2013-11-05 15:47:28 -0200

    fix the sign_up page
    The 'name' attribute nedded to be added to the permitted attributes on the strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def render_404
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << [:name, :email, :password, :password_confirmation]
   end
 end
 
