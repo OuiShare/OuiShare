a8bb079a03236913a8392e4cc06555c0e5eda810
Yann Kastelnik <yann@kastelnik.com>
2013-11-14 17:06:41 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131102194137) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authorizations", force: true do |t|
     t.integer  "user_id"
     t.string   "provider"
