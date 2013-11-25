802c9712a6bf5b2b6187b91e362d8c098ab0046d
Rafael Barboza <barboza.cardoso@gmail.com>
2013-11-25 11:30:09 -0200

    Adds the locale field to the user
    This will be used to store the language selected by each user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122174323) do
+ActiveRecord::Schema.define(version: 20131122203213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20131122174323) do
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false, null: false
     t.text     "bio"
+    t.string   "locale"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
