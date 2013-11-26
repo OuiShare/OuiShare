91cdca0cbe96f5504fc6e0a09614c8d42405b057
Guilherme Guerra <guerrinha@comum.org>
2013-11-26 12:26:03 -0200

    Applying layout topbanner

--- config/compass.rb
+++ config/compass.rb
@@ -4,6 +4,7 @@ css_dir = "public/assets"
 sass_dir = "app/assets/stylesheets"
 images_dir = "app/assets/images"
 javascripts_dir = "public/assets"
+fonts_dir = "app/assets/stylesheets/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
