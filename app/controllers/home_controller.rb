9ba5c297975d41b789a8c8ce2f14e1eab90d473d
Rafael Barboza <barboza.cardoso@gmail.com>
2013-11-28 17:16:52 -0200

    makes the homepage content match the locale
    The fr.yml is used to tell the rails_i18n gem that this locale is available

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
     @feed = Feedzirra::Feed.fetch_and_parse('http://ouishare.net/feed')
 
-    @top_banner = TopBanner.first
+    @top_banner = TopBanner.by_language(I18n.locale).first
+    @what_is_section = WhatIsSection.by_language(I18n.locale).first
   end
 end
