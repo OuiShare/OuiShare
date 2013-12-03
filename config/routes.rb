4072bce3af565b6acc6a17bfb9b0ca4513b0b2b6
Guilherme Guerra <guerrinha@comum.org>
2013-12-03 19:49:53 -0200

    Get involved layout

--- config/routes.rb
+++ config/routes.rb
@@ -78,12 +78,6 @@ OuiShare::Application.routes.draw do
   get 'knowledge/groups' => 'knowledge#groups', as: 'knowledge_groups'
   get 'knowledge/ouiki' => 'knowledge#ouiki', as: 'knowledge_ouiki'
 
-  get 'getinvolved' => 'getinvolved#index', as: 'getinvolved'
-  get 'press' => 'press#index', as: 'press'
-  get 'workus' => 'workus#index', as: 'workus'
-
-  get 'press' => 'press#index', as: 'press'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
