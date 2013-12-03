f1b69dc61268e812b9c4adb85971813645e53840
Guilherme Guerra <guerrinha@comum.org>
2013-12-03 20:30:41 -0200

    Press layout

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ OuiShare::Application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  #
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
