cfe44632539850c9979e46e4d32c59774e81a2ec
Guilherme Guerra <guerrinha@comum.org>
2013-12-03 20:30:41 -0200

    Press layout

--- config/routes.rb
+++ config/routes.rb
@@ -93,7 +93,6 @@ OuiShare::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-
   # Example resource route with options:
   #   resources :products do
   #     member do
