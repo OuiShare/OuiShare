31bc37751d3c4b1dd5f25a558f4967f13606e070
Guilherme Guerra <guerrinha@comum.org>
2013-12-03 20:30:41 -0200

    Press layout

--- config/routes.rb
+++ config/routes.rb
@@ -90,6 +90,7 @@ OuiShare::Application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
