4639355e5988f7a70b6fd10721930fab046344a6
Guilherme Guerra <guerrinha@comum.org>
2013-12-12 11:43:14 -0200

    Refactoring home css, html and javascript

--- app/assets/javascripts/oui_share/script.js
+++ app/assets/javascripts/oui_share/script.js
@@ -1,13 +1,13 @@
-$(document).foundation({
-  orbit: {
+$(document).foundation('orbit', {
     animation: 'slide',
-    timer_speed: 1000,
+    timer_speed: 10000,
     pause_on_hover: true,
+    resume_on_mouseout: false,
     animation_speed: 500,
-    navigation_arrows: false,
-    bullets: false,
-    next_on_click: true
-  }
+    slide_number: false,
+    next_class: 'orbit-next',
+    prev_class: 'orbit-prev',
+    timer: false
 });
 
 $(window).load(function() {
@@ -16,5 +16,13 @@ $(window).load(function() {
         window.location = lingua;
     }
     $("#l18n").change(language);
+
+    $(".orbit-bullets li.active").prepend("<span class='block'></span>");
+
+    $("#slideshow").on("orbit:after-slide-change", function(event, orbit) {
+        $(".orbit-bullets li").html("");
+        $(".orbit-bullets li.active").prepend("<span class='block'></span>");
+    });
+
 
 });
