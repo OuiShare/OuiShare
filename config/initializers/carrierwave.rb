adf86f2b06a0535331c36c5a458660599c65cdfd
Rafael Barboza <barboza.cardoso@gmail.com>
2013-12-12 16:23:04 -0200

    changes the endpoint again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ begin
       :aws_access_key_id      => ENV['aws.key'],       # required
       :aws_secret_access_key  => ENV['aws.secret'],       # required
       :region                 => 'us-west-2',  # optional, defaults to 'us-east-1'
-      :endpoint               => 'http://s3-us-west-2.amazonaws.com'
+      :endpoint               => 'http://ouishare.s3-external-3.amazonaws.com'
     }
     config.fog_directory  = 'ouishare'
   end
