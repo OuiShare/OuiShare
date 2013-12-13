04f3e80b7f48cf1a412c46bf122b673ad8ee4d2f
Rafael Barboza <barboza.cardoso@gmail.com>
2013-12-13 15:01:29 -0200

    changes the AWS info back to the OuiShare ones

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@ begin
       :provider               => 'AWS',       # required
       :aws_access_key_id      => ENV['aws.key'],       # required
       :aws_secret_access_key  => ENV['aws.secret'],       # required
-      :region                 => 'sa-east-1',  # optional, defaults to 'us-east-1'
+      :region                 => 'us-west-2',  # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'ouishare-test'
+    config.fog_directory  = 'ouishare'
   end
 rescue Exception => e
   Rails.logger.warn "Error loading settings: #{e}"
