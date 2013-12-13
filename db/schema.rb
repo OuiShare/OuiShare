36fc7261c954d1745d8a84e22388316b999e91bc
Rafael Barboza <barboza.cardoso@gmail.com>
2013-12-13 01:44:49 -0200

    adds the missing image field to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131213030210) do
+ActiveRecord::Schema.define(version: 20131213034052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20131213030210) do
     t.integer  "language_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   add_index "events", ["language_id"], name: "index_events_on_language_id", using: :btree
