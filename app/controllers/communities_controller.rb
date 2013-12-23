410f95f312c1cfa9577d2e567b3919932cd0a709
Rafael Barboza <barboza.cardoso@gmail.com>
2013-12-23 08:20:08 -0200

    finishes the community page backend

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,8 @@
 class CommunitiesController < ApplicationController
+  include AutoHtml
   def index
     @community_page = @current_language.community_page || CommunityPage.new
+    @community_page.main_text = auto_html(@community_page.main_text) { html_escape; image; youtube(:width => 400, :height => 250); link(:target => "_blank", :rel => "nofollow"); simple_format }
   end
 
   def local
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -79,6 +79,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   def has_top_internal? picture
-    ['Mission', 'GovernancePage', 'ValuePage', 'FaqPage'].include?(model.class.name)
+    ['Mission', 'GovernancePage', 'ValuePage', 'FaqPage', 'CommunityPage'].include?(model.class.name)
   end
 end
