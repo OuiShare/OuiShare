a3feeb916cf412a378fc24c19092d16274d1bc6c
Rafael Barboza <barboza.cardoso@gmail.com>
2013-12-27 23:11:54 -0200

    adds a missing file on the last 2 commits

---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :small do
+    process :resize_to_fill => [50, 50]
+  end
+
+end
+
