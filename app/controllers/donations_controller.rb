e78ac91287148a55c8002c116fd39c4ae6ca9d3f
Rafael Barboza <barboza.cardoso@gmail.com>
2013-12-30 08:57:46 -0200

    fix typo

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -6,7 +6,7 @@ class DonationsController < ApplicationController
     if params['value'] == 'custom'
       params['value'] = params['amount']
     end
-    Paypal.sandbox! if ENV['PAYPAL_SANDBOX'] || Raisl.env.development?
+    Paypal.sandbox! if ENV['PAYPAL_SANDBOX'] || Rails.env.development?
     donation = Donation.new
     payment = Payment.new(params['value'].to_i, params['currency'])
     payment.setup!(
