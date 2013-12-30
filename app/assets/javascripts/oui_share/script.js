5798f4282ae47cbdad92ada8e2dc74edb0b48118
Guilherme Guerra <guerrinha@comum.org>
2013-12-30 02:15:50 -0200

    force number on custom value donation

--- app/assets/javascripts/oui_share/script.js
+++ app/assets/javascripts/oui_share/script.js
@@ -39,4 +39,26 @@ $(window).load(function() {
         $(this).val("");
     });
 
+
+});
+
+jQuery(function () {
+    jQuery("#custom").keydown(function(event) {
+        // Allow: backspace, delete, tab and escape
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+             // Allow: Ctrl+A
+             (event.keyCode == 65 && event.ctrlKey === true) ||
+             // Allow: home, end, left, right
+             (event.keyCode >= 35 && event.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if ( event.shiftKey|| (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 ) )
+            {
+                event.preventDefault();
+            }
+        }
+    });
 });
