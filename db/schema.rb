9f4c4675d335e10ee1a833e1301e9573a7e85d41
Rafael Barboza <barboza.cardoso@gmail.com>
2014-01-07 00:38:55 -0200

    adds the title to the users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131230141557) do
+ActiveRecord::Schema.define(version: 20140107022959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -507,6 +507,7 @@ ActiveRecord::Schema.define(version: 20131230141557) do
     t.string   "github_url"
     t.string   "linkedin_url"
     t.integer  "profile_type"
+    t.string   "title"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
