ce232b10183ded3fdc62a709e1d90121bea5769f
Guilherme Guerra <guerrinha@comum.org>
2014-01-27 11:28:23 -0200

    Fix: #52 - sticky footer

--- app/assets/javascripts/oui_share/script.js
+++ app/assets/javascripts/oui_share/script.js
@@ -78,3 +78,16 @@ jQuery(function () {
         }
     });
 });
+
+$(window).bind("load", function () {
+    var footer = $("#footer");
+    var pos = footer.position();
+    var height = $(window).height();
+    height = height - pos.top;
+    height = height - footer.height();
+    if (height > 0) {
+        footer.css({
+            'margin-top': height + 'px'
+        });
+    }
+});
