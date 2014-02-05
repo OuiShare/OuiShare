bf4a6da5bb2c3d00050b60366b2f5b3b8bb00416
Rafael Barboza <barboza.cardoso@gmail.com>
2014-02-05 14:10:20 -0200

    fix the magazine feed url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,9 @@ class HomeController < ApplicationController
 
   def index
     if @current_language.slug == 'en'
-      @feed = Feedzirra::Feed.fetch_and_parse('http://ouishare.net/feed')
+      @feed = Feedzirra::Feed.fetch_and_parse('http://magazine.ouishare.net/feed')
     else
-      @feed = Feedzirra::Feed.fetch_and_parse("http://ouishare.net/#{@current_language.slug}/feed")
+      @feed = Feedzirra::Feed.fetch_and_parse("http://magazine.ouishare.net/#{@current_language.slug}/feed")
     end
 
     @top_banner = @current_language.top_banner || TopBanner.new
