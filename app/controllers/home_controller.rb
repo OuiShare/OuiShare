8c860c164a39535fa6828db480f81d7b19236d3b
Rafael Barboza <barboza.cardoso@gmail.com>
2014-02-11 20:54:31 -0200

    adds cache to the magazine articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,6 @@ class HomeController < ApplicationController
   include AutoHtml
 
   def index
-    if @current_language.slug == 'en'
-      @feed = Feedzirra::Feed.fetch_and_parse('http://magazine.ouishare.net/feed')
-    else
-      @feed = Feedzirra::Feed.fetch_and_parse("http://magazine.ouishare.net/#{@current_language.slug}/feed")
-    end
-
     @top_banner = @current_language.top_banner || TopBanner.new
     @what_is_section = @current_language.what_is_section || WhatIsSection.new
     @hot_projects_section = @current_language.hot_projects_section || HotProjectsSection.new
