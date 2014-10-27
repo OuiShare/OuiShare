fb3852bafeb24b90d2cac93cc6059e3c392e7b50
Karim Mortabit <karim.mortabit@gmail.com>
2014-10-27 11:01:12 +0100

    Overwrites devise update method w/ pwd conf.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,6 +12,29 @@ class RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  def update
+    # For Rails 4
+    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
+    # For Rails 3
+    # account_update_params = params[:user]
+
+    # required for settings form to submit when password is left blank
+    if account_update_params[:password].blank?
+      account_update_params.delete("password")
+      account_update_params.delete("password_confirmation")
+    end
+
+    @user = User.find(current_user.id)
+    if @user.update_attributes(account_update_params)
+      set_flash_message :notice, :updated
+      # Sign in the user bypassing validation in case their password changed
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
   protected
 
   def after_sign_up_path_for(resource)
