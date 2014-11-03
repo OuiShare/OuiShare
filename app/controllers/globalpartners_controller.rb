38117478446c27436be4b30582d87b7cc1394a63
spuyet <spuyet@student.42.fr>
2014-11-03 15:39:24 +0100

    block access to global-partners for guets and no admin

--- app/controllers/globalpartners_controller.rb
+++ app/controllers/globalpartners_controller.rb
@@ -1,6 +1,11 @@
 class GlobalpartnersController < ApplicationController
-	
-	def index
+
+  before_action :authenticate_user!
+
+  def index
+    unless current_user.admin
+      return redirect_to root_path
+    end
 	end
 
 end
