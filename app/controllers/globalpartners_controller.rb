6aac35e444ae70c51b319019c8c7aac59ae0d696
Karim Mortabit <karim.mortabit@gmail.com>
2014-11-07 11:23:55 +0100

    Unlock partners page and fixes HP partners section

--- app/controllers/globalpartners_controller.rb
+++ app/controllers/globalpartners_controller.rb
@@ -3,9 +3,6 @@ class GlobalpartnersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    unless current_user.admin
-      return redirect_to root_path
-    end
 	end
 
 end
