68e438adbaab8b85b7ce0d5efa4641c3f89bcf26
karnaud <karol.arnaud@gmail.com>
2015-09-11 12:41:45 +0200

    reput right domain name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ OuiShare::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.default_url_options = {:host => 'ouishare.net'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => ENV['SMTP_ADDRESS'],
