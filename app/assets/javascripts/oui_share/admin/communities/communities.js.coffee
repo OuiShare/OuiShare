6b3f88009066f67762c9b8b15000fc43af647b40
Gabriel <gabriel.bidula@gmail.com>
2017-02-08 11:20:36 +0100

    Add autocomplete to projects, community users

--- app/assets/javascripts/oui_share/admin/communities/communities.js.coffee
+++ app/assets/javascripts/oui_share/admin/communities/communities.js.coffee
@@ -13,3 +13,18 @@ jQuery ->
         $('.event_community').append("<div class='event-community-value' id='event-"+id+"'><span class='remove-event-community'>x</span>" +value+ "</div>")
         community_event_ids.push(id)
         $('#community_events_add').val community_event_ids
+
+  community_user_ids = []
+  $('#community_user_name').autocomplete
+    source: $('#community_user_name').data('autocomplete-source')
+    select: (event, ui) ->
+      event.preventDefault()
+      $(this).val ui.item.label
+      $('#community_users_add').val ui.item.value
+      id = $('#community_users_add').val()
+      value = $('#community_user_name').val()
+      $(this).val('')
+      if (value)
+        $('.user_community').append("<div class='user-community-value' id='user-"+id+"'><span class='remove-user-community'>x</span>" +value+ "</div>")
+        community_user_ids.push(id)
+        $('#community_users_add').val community_user_ids
--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -3,7 +3,17 @@ module Admin
     inherit_resources
     belongs_to :language
 
-    def autocomplete
+    def autocomplete_users
+      if params[:term].present?
+        users = User.order(:name).where("name ILIKE ? OR fname ILIKE ?", "%#{params[:term]}%", "%#{params[:term]}%")
+
+        respond_to do |format|
+          format.json { render json: users.map{|u| { :label => u.full_name, :value => u.id } } }
+        end
+      end
+    end
+
+    def autocomplete_events
       if params[:term].present?
         events = Event.order(:title).where("title ILIKE ?", "%#{params[:term]}%")
 
@@ -21,6 +31,8 @@ module Admin
     end
 
     def update
+      add_users if params[:community_users_add].present?
+      remove_users if params[:community_users_remove].present?
       add_events if params[:community_events_add].present?
       remove_events if params[:community_events_remove].present?
 
@@ -61,6 +73,18 @@ module Admin
     end
 
     private
+    def add_users
+      user_ids = params[:community_users_add].split(",")
+      users = User.find(user_ids)
+      resource.users << users
+    end
+
+    def remove_users
+      user_ids = params[:community_users_remove].split(",")
+      users = User.find(user_ids)
+      resource.users.delete(users)
+    end
+
     def add_events
       event_ids = params[:community_events_add].split(",")
       events = Event.find(event_ids)
